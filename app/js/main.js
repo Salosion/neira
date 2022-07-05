7c2d40339b5120b101c3fae146ea005d5a8ed2ae
Salosion <denis-stepurka@mail.ru>
2022-07-05 11:20:50 +0600

    added smooth scrolling, sticky header, anchor links

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  
+  $(".menu a").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+});
